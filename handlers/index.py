bcd34c63f332ec78f87386a59e2374e71d7870f8
Lsplastic <Lsplastic@github.com>
2017-09-12 18:22:18 +0800

    use beauitiful soup
    edition of beautiful soup

--- handlers/index.py
+++ handlers/index.py
@@ -4,6 +4,8 @@ import tornado.web
 import urllib
 import urllib2
 import re
+from bs4 import BeautifulSoup
+
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
@@ -13,7 +15,12 @@ class IndexHandler(tornado.web.RequestHandler):
         inputdata = self.get_argument("input")
         response  = urllib.urlopen(inputdata)
         html      = response.read()
-        dr=re.compile(r'<[^>]+>',re.S)
-        rehtml=dr.sub('',html)
+
+        soup      = BeautifulSoup(html)
+        rehtml=soup.get_text()
+
+        #dr=re.compile(r'<[^>]+>',re.S)
+        #rehtml=dr.sub('',html)
+
         self.write(rehtml)
         
